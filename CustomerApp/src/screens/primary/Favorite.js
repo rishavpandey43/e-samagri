883f29c1c25a2bf4784db3838607e3afe6ef2db2
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-04-19 18:04:51 +0530

    :sparkles: feat: complete favorite screen

--- CustomerApp/src/screens/primary/Favorite.js
+++ CustomerApp/src/screens/primary/Favorite.js
@@ -4,22 +4,64 @@ import {
   StyleSheet,
   View,
   Text,
-  Button,
   FlatList,
   SafeAreaView,
+  ActivityIndicator,
 } from 'react-native';
-import {Header, Card} from 'react-native-elements';
+import {Header, Image, Button} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import mainStyles from '../../styles/mainStyle';
+import variables from '../../styles/variables';
 
 class FavoriteScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      favorite: true,
+    };
   }
 
+  removeFavorite = () => {};
+
   render() {
+    const favoriteStore = () => {
+      return (
+        <View style={[mainStyles.row, {marginTop: 10, marginBottom: 10}]}>
+          <View style={mainStyles.col3}>
+            <Image
+              source={{uri: 'https://via.placeholder.com/100'}}
+              style={{width: '100%', height: '100%'}}
+              PlaceholderContent={<ActivityIndicator />}
+            />
+          </View>
+          <View style={mainStyles.col7}>
+            <View>
+              <Text style={{fontSize: 18}}>Sellet Store 1</Text>
+              <Text style={{color: '#555'}}>
+                Dummy Address, ksbc,1234, dto, In
+              </Text>
+            </View>
+            <View style={{marginTop: 10}}>
+              <Button title="Shop from this store" type="outline" />
+            </View>
+          </View>
+          <View
+            style={[
+              mainStyles.col2,
+              {justifyContent: 'center', alignItems: 'center'},
+            ]}>
+            <Icon
+              name={`${this.state.favorite ? 'heart' : 'heart-o'}`}
+              size={25}
+              onPress={() => {}}
+              color={variables.mainThemeColor}
+            />
+          </View>
+        </View>
+      );
+    };
+
     return (
       <View>
         <Header
@@ -34,7 +76,7 @@ class FavoriteScreen extends Component {
             />
           }
           centerComponent={{
-            text: 'My Favorite',
+            text: 'My Favorite Stores',
             style: {color: '#fff'},
           }}
           rightComponent={<Icon name="heart" color="#FFF" size={30} />}
@@ -45,7 +87,10 @@ class FavoriteScreen extends Component {
         />
         <ScrollView>
           <View style={[mainStyles.container, {marginBottom: 100}]}>
-            <FlatList />
+            {favoriteStore()}
+            {favoriteStore()}
+            {favoriteStore()}
+            {favoriteStore()}
           </View>
         </ScrollView>
       </View>

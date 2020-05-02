6346eff77256c693de1c6bc95d21eefab2bff8df
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-05-02 15:47:55 +0530

    :bug: fix the typo and logic in helper function

---
+++ grocery-backend/util/constant.js
@@ -0,0 +1,42 @@
+exports.alertNotificationForCustomer = [
+  {
+    title: "Order Update",
+    body: "Your order has been successfully processed by seller.",
+    value: "prc",
+  },
+  {
+    title: "Order Update",
+    body: "Your order has been successfully processed by seller.",
+    value: "ofd",
+  },
+  {
+    title: "Order Update",
+    body:
+      "Your order has been successfully delivered, Thank you for shopping with us.",
+    value: "del",
+  },
+  {
+    title: "Order Update",
+    body: "We're sorry, your order has been cancelled by seller.",
+    value: "can",
+  },
+];
+
+exports.alertNotificationForSeller = [
+  {
+    title: "New Order",
+    body: "Your order has been successfully processed by seller.",
+    value: "nwo",
+  },
+  {
+    title: "Order Update",
+    body: "Your order has been successfully processed by seller.",
+    value: "ofd",
+  },
+  {
+    title: "Order Update",
+    body:
+      "Your order has been successfully delivered, Thank you for shopping with us.",
+    value: "del",
+  },
+];
